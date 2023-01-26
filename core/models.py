470098dd0ea39993b1330b57aa3518c9c186d52c
Evgeniy <eugtrader@gmail.com>
2023-01-26 18:21:09 +0300

    first

---
+++ core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+
+    # username = models.CharField(null=True, max_length=50)
+    # email = models.CharField(null=True, max_length=50)
+    # first_name = models.CharField(null=True, max_length=50)
+    # last_name = models.CharField(null=True, max_length=50)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ["username"]
