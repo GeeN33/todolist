85e40fe3813981ea5fac70a178a68f424a82d89b
Evgeniy <eugtrader@gmail.com>
2023-01-30 14:17:32 +0300

    first7

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,8 @@
 # образ на основе которого создаём контейнер
 FROM python:3.9.6-alpine
 
-# рабочая директория внутри проекта
-WORKDIR /usr/src/code
+COPY .todolist/srv/www/todolist .
+WORKDIR /srv/www/todolist
 
-# переменные окружения для python
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
-
-RUN apk update \
-    && apk add postgresql-dev gcc python3-dev musl-dev
-
-# устанавливаем зависимости
-RUN pip install --upgrade pip
-COPY ./requirements.txt .
 RUN pip install -r requirements.txt
-
-COPY ./entrypoint.sh .
-
-# копируем содержимое текущей папки в контейнер
-COPY . .
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,44 +1,12 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+sleep 10
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
-python manage.py flush --no-input
 python manage.py migrate
+python manage.py createcachetable
+python manage.py collectstatic  --noinput
+gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#sleep 10
-
-#python manage.py migrate --noinput
-#gunicorn tdolist.wsgi:application --bind 0.0.0.0:8000
-#
-##python manage.py migrate
-##python manage.py createcachetable
-##python manage.py collectstatic  --noinput
-##gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
-#
-#exec "$@"
 
 
